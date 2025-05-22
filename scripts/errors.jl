6cb668bf71f18dde76899f119e5c030d709701f7
Lukman Aliyu Jibril <lukman.j.aliyu@gmail.com>
2025-05-22 12:31:38 +0100

    update errors script

--- scripts/errors.jl
+++ scripts/errors.jl
@@ -1,2 +1,37 @@
 ## Error Handling
 # try-catch
+function friendly_divide(x, y)
+    try
+        return x ÷ y
+    catch e
+        if e isa DivideError
+            #println("Error: Division by zero!")
+            @warn("Error: Division by zero!")
+            @info("Please provide a non-zero denominator.")
+        elseif e isa ArgumentError
+            #println("Error: Invalid argument!")
+            @warn("Error: Invalid argument!")
+            @info("Please check the input values.")
+        elseif e isa MethodError
+            #println("Error: Method not found!")
+            @warn("Error: Method not found!")
+            @info("Please check the method signature.")
+        else
+        @error("An unexpected error occurred: ")
+        end
+        return NaN  # Return NaN in case of an error
+    end
+end
+# Example usage
+result = friendly_divide(10, 0)
+println("Result: ", result)  # Output: Result: NaN
+
+
+# Using throw
+
+function finite_log(x)
+    if x <= 0
+        throw(ArgumentError("Input must be positive."))
+    end
+    return log(x)
+end
